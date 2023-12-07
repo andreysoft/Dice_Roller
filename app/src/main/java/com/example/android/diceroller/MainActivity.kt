f27fdb22333de6a8a7620fbb687968c47ecde3d7
Andrey <fromjfktosfo@gmail.com>
2023-12-07 18:56:49 -0500

    enable viewbinding and adding Toast

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.android.diceroller.databinding.ActivityMainBinding
+import kotlin.time.Duration
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        binding.rollButton.setText(R.string.roll1)
+
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
