149a70ca6c3bd222566030a3f9b5373828644413
Andrey <fromjfktosfo@gmail.com>
2023-12-08 20:09:55 -0500

    adding new image files

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import com.example.android.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 import kotlin.time.Duration
 
 class MainActivity : AppCompatActivity() {
@@ -23,9 +24,15 @@ class MainActivity : AppCompatActivity() {
         binding.rollButton.setText(R.string.roll1)
 
         binding.rollButton.setOnClickListener {
-            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
+
+    }
+
+    private fun rollDice() {
+        binding.resultText.setText(Random.nextInt(1,7).toString())
 
     }
 }
